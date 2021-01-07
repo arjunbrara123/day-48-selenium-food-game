04b5725d4974e596cc8b0cca4855059a996e262f
Arjun <arjunbrara@hotmail.co.uk>
2021-01-07 23:18:12 +0000

    Dynamic number of events

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ driver.get("https://www.python.org/")
 
 events = {}
 
-for event_no in range(5):
-    event_date = driver.find_element_by_xpath(f'//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[{event_no+1}]/time').text
-    event_name = driver.find_element_by_xpath(f'//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[{event_no+1}]/a').text
-    events[event_no] = {
-        "time": event_date,
-        "name": event_name
-    }
+for event_no in range(9):
+    try:
+        event_date = driver.find_element_by_xpath(f'//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[{event_no+1}]/time').text
+        event_name = driver.find_element_by_xpath(f'//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[{event_no+1}]/a').text
+        events[event_no] = {
+            "time": event_date,
+            "name": event_name
+        }
+    except:
+        break
 
 print(events)
 
